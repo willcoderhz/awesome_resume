bc2088fac9ac94a198691819951d6f1a122127aa
雅哲 <huangyazhe@gmail.com>
2023-10-28 22:07:48 +0800

    1、添加路由组件 2、增加首页模块

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import ResumeEditor from './pages/ResumeEditor/ResumeEditor'
+import {  Route,Routes } from "react-router-dom";
+
+
+import Home from "./views/home/Home.tsx";
+
+
 
 function App() {
 
 
   return (
-    <>
-      <ResumeEditor/>
-      {/*测试 tailwindcss 可以去掉 */}
-      {/*<h1 className="text-3xl font-bold underline">asdfafa</h1>*/}
-    </>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<ResumeEditor />} />
+      </Routes>
+
+
+
+    // <>
+    //   <ResumeEditor/>
+    //   {/*测试 tailwindcss 可以去掉 */}
+    //   {/*<h1 className="text-3xl font-bold underline">asdfafa</h1>*/}
+    // </>
   )
 }
 
---
+++ src/components/home/Mainc.tsx
@@ -0,0 +1,113 @@
+const Mainc = () => {
+    return (
+
+        <div>
+            <section>
+                <div className="relative px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+                    <div className="text-center">
+                        <h2 className="font-title relative z-[2] mx-auto text-[clamp(2rem,6vw,4.5rem)] font-black leading-none">
+                <span className="motion-reduce:!opacity-100">
+                  他们  这样的学习方式
+                </span>
+                        </h2> <p className="font-title relative z-[2] py-4 font-light text-base-content/60 md:text-3xl">
+                        辅导学员完成就业，快速提升技术水平。
+                    </p>
+                    </div>
+
+                    <div className="relative mx-auto max-w-7xl">
+                        <div className="mx-auto grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
+                            <div className="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                                <div className="flex flex-1 flex-col justify-between bg-white p-6">
+                                    <div className="flex-1">
+                                        <a href="#" className="mt-2 block">
+                                            <p className="text-xl font-semibold text-neutral-600">1V1陪跑服务计划</p>
+                                            <p className="mt-3 text-base text-gray-500 ">我目前大三，感谢我的私教规划和帮助，最近这段时间成长特别快，没想到自己可以短期会这么多技术，现在已经找到实习工作，这里非常感谢私教的帮助.</p>
+                                        </a>
+                                    </div>
+                                    <div className="mt-6 flex items-center">
+                                        <div className="shrink-0">
+                                            <a href="">
+                                                <img className="h-10 w-10 rounded-full"
+                                                     src="~/assets/images/testimonial-1.png" alt=""/>
+                                            </a>
+                                        </div>
+                                        <div className="ml-3">
+                                            <p className="text-sm font-medium text-neutral-600">
+                                                <a href="#" className="hover:underline"> 王同学</a>
+                                            </p>
+                                            <div className="flex space-x-1 text-sm text-gray-500">
+                                                <time dateTime="2023-06-13"> 23年6月13日</time>
+                                                <span aria-hidden="true"> · </span>
+                                                <span> 留言 </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                                <div className="flex flex-1 flex-col justify-between bg-white p-6">
+                                    <div className="flex-1">
+                                        <a href="" className="mt-2 block">
+                                            <p className="text-xl font-semibold text-neutral-600">1V1陪跑服务计划</p>
+                                            <p className="mt-3 text-base text-gray-500">我目前大四，之前几乎只会语法，也没有实习经验，自己没有实战项目，找工作非常迷茫，通过教练的规划，自己现在比身边的人，技术水平强上很多，非常感谢私教的指导和帮助.</p>
+                                        </a>
+                                    </div>
+                                    <div className="mt-6 flex items-center">
+                                        <div className="shrink-0">
+                                            <a href="#">
+                                                <img className="h-10 w-10 rounded-full"
+                                                     src="~/assets/images/testimonial-2.png" alt=""/>
+                                            </a>
+                                        </div>
+                                        <div className="ml-3">
+                                            <p className="text-sm font-medium text-neutral-600">
+                                                <a href="#" className="hover:underline"> 崔同学 </a>
+                                            </p>
+                                            <div className="flex space-x-1 text-sm text-gray-500">
+                                                <time dateTime="2023-06-13"> 23年6月13日</time>
+                                                <span aria-hidden="true"> · </span>
+                                                <span> 留言 </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                                <div className="flex flex-1 flex-col justify-between bg-white p-6">
+                                    <div className="flex-1">
+                                        <a href="#" className="mt-2 block">
+                                            <p className="text-xl font-semibold text-neutral-600">简历优化</p>
+                                            <p className="mt-3 text-base text-gray-500">以前简历面试机会很少，比较迷茫不知道什么原因，通过私教指导优化最近简历曝光很好，有很多面试机会，老师的面试建议和面试思路的讲解，对我面试帮助很大，感谢我的私教.</p>
+                                        </a>
+                                    </div>
+                                    <div className="mt-6 flex items-center">
+                                        <div className="shrink-0">
+                                            <a href="#">
+                                                <img className="h-10 w-10 rounded-full"
+                                                     src="~/assets/images/testimonial-3.png" alt=""/>
+                                            </a>
+                                        </div>
+                                        <div className="ml-3">
+                                            <p className="text-sm font-medium text-neutral-600">
+                                                <a href="#" className="hover:underline"> 李同学 </a>
+                                            </p>
+                                            <div className="flex space-x-1 text-sm text-gray-500">
+                                                <time dateTime="2023-06-13"> 23年6月13日</time>
+                                                <span aria-hidden="true"> · </span>
+                                                <span> 留言 </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+        </div>
+
+    );
+}
+export default Mainc;
